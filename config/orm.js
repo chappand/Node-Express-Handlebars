6cecead62f7f7196114b4ad908deeb2e2d5d6842
Andrew Chappell <chappand@gmail.com>
2021-03-09 22:19:38 -0500

    added the update query to the orm.js file.

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,31 @@
 const connection = require('./connection.js');
 
+const printQuestionMarks = (num) => {
+    const arr = [];
+  
+    for (let i = 0; i < num; i++) {
+      arr.push('?');
+    }
+  
+    return arr.toString();
+  };  
+
+  const objToSql = (ob) => {
+    const arr = [];
+
+    for (const key in ob) {
+        let value = ob[key];
+        if (Object.hasOwnProperty.call(ob, key)) {
+          if (typeof value === 'string' && value.indexOf(' ') >= 0) {
+            value = `'${value}'`;
+          }
+          arr.push(`${key}=${value}`);
+        }
+      }
+    
+      return arr.toString();
+    };
+
 const orm = {
     all(cb) {
       const queryString = `SELECT * FROM burgers;`;
@@ -28,5 +54,23 @@ const orm = {
           cb(result);
         });
       },
+      update(objColVals, condition, cb) {
+        let queryString = `UPDATE burgers`;
+    
+        queryString += ' SET ';
+        queryString += objToSql(objColVals);
+        queryString += ' WHERE ';
+        queryString += condition;
+    
+        console.log(queryString);
+        connection.query(queryString, (err, result) => {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+      }
+    };
 
 module.exports = orm;
