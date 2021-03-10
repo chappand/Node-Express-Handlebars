a8eb74ad50a21bb98321fc6dde2381b058358606
Andrew Chappell <chappand@gmail.com>
2021-03-09 22:30:31 -0500

    added controllers folder and burgers_controller.js file. required express and burger.js from models.

--- models/burger.js
+++ models/burger.js
@@ -4,7 +4,6 @@ const burger = {
     all(cb) {
       orm.all((res) => cb(res));
     },
-    // The variables cols and vals are arrays.
     create(cols, vals, cb) {
       orm.create(cols, vals, (res) => cb(res));
     },
