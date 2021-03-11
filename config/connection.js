4b56119e3a7147b4f5de9e22f183064f2eca2692
Andrew Chappell <chappand@gmail.com>
2021-03-10 22:12:38 -0500

    Trying to get buttons to work

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,18 @@
-// Set up MySQL connection.
 const mysql = require('mysql');
-
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  // NOTE: Be sure to add your MySQL password here!
-  password: '',
-  database: 'burgers_db',
+let connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+  connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: "",
+    database: 'burgers_db',
 });
+}
 
-// Make connection.
 connection.connect((err) => {
   if (err) {
     console.error(`error connecting: ${err.stack}`);
@@ -19,5 +21,4 @@ connection.connect((err) => {
   console.log(`connected as id ${connection.threadId}`);
 });
 
-// Export connection for our ORM to use.
 module.exports = connection;
